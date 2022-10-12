e384372a6957e93fefd9c0868c3022bba92beb8b
yifeng.tao <yifeng.tao@lkcoffee.com>
2022-10-12 21:28:54 +0800

    seventh day: JSON Request

--- src/controller.rs
+++ src/controller.rs
@@ -1,6 +1,15 @@
-use actix_web::{web, App, HttpRequest, HttpServer, Responder, http::KeepAlive, HttpResponse};
-use openssl::ssl::{SslAcceptor, SslFiletype, SslMethod};
+use actix_web::{web, App, HttpServer};
 
 mod home;
+
+#[actix_web::main]
+pub async fn format_info_instance() -> std::io::Result<()> {
+  HttpServer::new(|| {
+    App::new().route("/json", web::post().to(home::format_info))
+  })
+  .bind("127.0.0.1:8080")?
+  .run()
+  .await
+}
 
 
--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,6 @@ mod controller;
 
 
 fn main() {
-
+  // format json by serde
+  controller::format_info_instance().unwrap();
 }
